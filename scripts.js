6398bf3a4461a94debbdbedff717d031d6a91150
Jonathan Soares <jonthan.soares10@gmail.com>
2023-02-02 19:59:24 -0300

    continuação

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Variáveis = Um espaço da memória do computador que guardamos algo (numeros, letra, imagem)
+
+// Função = Um trecho de código que só é executado quando é chamado
+
+let abacate = "Já deu o like?"
+
+console.log(abacate)
+console.log ()
+
+function mario() {
+    console.log("Estou dentro da função")
+}
