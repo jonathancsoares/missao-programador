f712a47ef6350d7f42035f269c3e9c7c42f95d73
Jonathan Soares <jonthan.soares10@gmail.com>
2023-02-03 20:27:34 -0300

    Finalização do projeto

--- scripts.js
+++ scripts.js
@@ -2,11 +2,39 @@
 
 // Função = Um trecho de código que só é executado quando é chamado
 
-let abacate = "Já deu o like?"
+let chave = "eb55941e4aa152eddc6b5a825729a8e6"
 
-console.log(abacate)
-console.log ()
+function colocarNaTela(dados) {
+    console.log(dados)
 
-function mario() {
-    console.log("Estou dentro da função")
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+
+    document.querySelector(".temp").innerHTML = 
+    Math.floor(dados.main.temp) + " ºC"
+
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
 }
+
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+    cidade +
+    "&appid=" +
+    chave +
+    "&lang=pt_br" +
+     "&units=metric")
+    .then( resposta => resposta.json())   
+
+    colocarNaTela(dados)
+}
+
+
+
+function cliqueiNoBotao() {
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+} 
